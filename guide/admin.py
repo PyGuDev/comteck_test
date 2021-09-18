7adb63dba3300df15c2d243962c5f0cfd34c6930
pygudev <gubaev.andreb@gmail.com>
2021-09-18 23:28:41 +0500

    Сделал админку для приложения Guide

--- guide/admin.py
+++ guide/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Guide, GuideItem, GuideVersion
+
+
+class GuideVersionTabularInline(admin.TabularInline):
+    model = GuideVersion
+    fields = ['title', 'date_created', 'add_button']
+    readonly_fields = ('add_button',)
+    extra = 0
+
+    def add_button(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Добавить</a>',
+            reverse('admin:guide_guideversion_change', args=[obj.pk])
+        )
+
+    def get_formset(self, request, obj=None, **kwargs):
+        labels = {'add_button': 'Добавить элементы'}
+        kwargs.update({'labels': labels})
+        return super().get_formset(request, obj, **kwargs)
+
+
+@admin.register(Guide)
+class GuideAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'short_name']
+    inlines = [GuideVersionTabularInline]
+
+
+@admin.register(GuideItem)
+class GuideItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class GuideItemTabularInline(admin.TabularInline):
+    model = GuideItem
+    fields = ['code_item', 'value_item']
+    extra = 0
+
+
+@admin.register(GuideVersion)
+class GuideVersionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    inlines = [GuideItemTabularInline]
