f48fdaeee7be5c4a4c965ffcb537a589e7a1e90f
pygudev <gubaev.andreb@gmail.com>
2021-09-19 15:19:30 +0500

    Добавил urls путь к ListGuideItems

--- guide/urls.py
+++ guide/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ListGuideAPIView
+from .views import ListGuideAPIView, ListGuideItemAPIView
 
 
 urlpatterns = [
     path('guide/', ListGuideAPIView.as_view(), name='list_guide'),
+    path('guide/<slug:pk>/items/current/', ListGuideItemAPIView.as_view(), name='list_guide_item')
 ]
