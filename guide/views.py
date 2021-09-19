7a06e9bc42ea0452f660f49ad1a1e34e2ee13072
pygudev <gubaev.andreb@gmail.com>
2021-09-19 11:45:03 +0500

    Добавил описание и сделал филтрацию по дате в ListGuideAPIView

--- guide/views.py
+++ guide/views.py
@@ -1,8 +1,24 @@
+from django.core.exceptions import ValidationError
 from rest_framework.generics import ListAPIView
 from .serializers import ListGuideSerializer
 from .models import Guide
 
 
 class ListGuideAPIView(ListAPIView):
+    """
+    Вывод списка справочников
+
+    Можно получить актуальные справочники на указанную дату
+    ?date=2021-10-10 в таком формате
+    """
     serializer_class = ListGuideSerializer
-    queryset = Guide.objects.all()
+
+    def get_queryset(self):
+        queryset = Guide.objects.all()
+        date = self.request.query_params.get('date')
+        if date:
+            try:
+                queryset = queryset.filter(guide_version__date_created__gte=date)
+            except ValidationError:
+                pass
+        return queryset
