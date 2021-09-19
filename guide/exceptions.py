5fbf76b043172ab7ddb7bb8163bf159343b61050
pygudev <gubaev.andreb@gmail.com>
2021-09-20 01:12:01 +0500

    В BadRequestError добавил параметр data

--- guide/exceptions.py
+++ guide/exceptions.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from rest_framework import status
 from rest_framework.exceptions import APIException
 
@@ -5,6 +7,8 @@ from rest_framework.exceptions import APIException
 class BadRequestError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
 
-    def __init__(self, message: str, code: str):
+    def __init__(self, message: str, code: str, data: Optional[dict] = None):
         detail = {'message': message, 'code': code}
+        if data:
+            detail.update(data)
         super().__init__(detail=detail)
