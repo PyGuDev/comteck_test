c590ec76b180bbdb4690e5ffc432f5bfb9f7a3ee
pygudev <gubaev.andreb@gmail.com>
2021-09-19 23:44:23 +0500

    Добавил тесты

--- guide/tests.py
+++ guide/tests.py
@@ -47,7 +47,43 @@ class GuideTestCase(TestCase):
         list_guide_item_url = reverse('list_guide_item_current',
                                       kwargs={'guide_pk': '34c40aeb-5866-4240-915d-c94526df3f22'})
         response = self.client.get(list_guide_item_url)
+
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_validate_list_item_version_current(self):
+        list_guide_item_url = reverse('list_guide_item_current',
+                                      kwargs={'guide_pk': '34c40aeb-5866-4240-915d-c94526df3fbb'})
+        send_data = [
+            {
+                "code_item": "FFF",
+                "value_item": "Темп"
+            },
+            {
+                "code_item": "SAS",
+                "value_item": "Тошноты"
+            }
+        ]
+
+        response = self.client.put(list_guide_item_url, data=send_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_validate_list_item_version_current_error(self):
+        list_guide_item_url = reverse('list_guide_item_current',
+                                      kwargs={'guide_pk': '34c40aeb-5866-4240-915d-c94526df3fbb'})
+        send_data = [
+            {
+                "code_item": "F2F",
+                "value_item": "Темп"
+            },
+            {
+                "code_item": "SAS",
+                "value_item": "Тошноты"
+            }
+        ]
+
+        response = self.client.put(list_guide_item_url, data=send_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'message': 'Item code F2F invalid', 'code': 'invalid'})
 
     def test_get_list_item_selected_version(self):
         list_guide_item_url = reverse('list_guide_item_selected_version',
@@ -64,4 +100,39 @@ class GuideTestCase(TestCase):
         response = self.client.get(list_guide_item_url)
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_valid_list_item_selected_version(self):
+        list_guide_item_url = reverse('list_guide_item_selected_version',
+                                      kwargs={'version_pk': 'fc0d6d70-27e2-4b84-9074-ceb2904cbed7'})
+        send_data = [
+            {
+                "code_item": "FFF",
+                "value_item": "Темп"
+            },
+            {
+                "code_item": "SAS",
+                "value_item": "Тошноты"
+            }
+        ]
+
+        response = self.client.put(list_guide_item_url, data=send_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_valid_list_item_selected_version_error(self):
+        list_guide_item_url = reverse('list_guide_item_selected_version',
+                                      kwargs={'version_pk': 'fc0d6d70-27e2-4b84-9074-ceb2904cbed7'})
+        send_data = [
+            {
+                "code_item": "FFF",
+                "value_item": "Темп"
+            },
+            {
+                "code_item": "SAA",
+                "value_item": "Тошноты"
+            }
+        ]
+
+        response = self.client.put(list_guide_item_url, data=send_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {'message': 'Item code SAA invalid', 'code': 'invalid'})
 
